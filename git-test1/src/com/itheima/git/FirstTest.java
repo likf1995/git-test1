22f7488c50cd9c9f69aa36a341c07c34b9b08a3d
likaifeng <likaifeng@qq.com>
2018-12-18 22:46:15 +0800

    第一次创建

---
+++ git-test1/src/com/itheima/git/FirstTest.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("git11111");
+        System.out.println("git11111");
+        System.out.println("git11111");
+        System.out.println("git11111");
+        System.out.println("git11111");
+    }
+}
